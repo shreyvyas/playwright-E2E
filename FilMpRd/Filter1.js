58e3b82d03e224a1ef975743ff638e038c6db5ae
shreyvyas <shreyvyas@gmail.com>
2026-01-10 14:28:43 +0530

    add initial implementation of filter and map functions

---
+++ FilMpRd/Filter1.js
@@ -0,0 +1,11 @@
+//filter: it returns an array
+//condition
+
+const nums = [1,2,3,4,5];
+const filter1 = nums.filter(n => n%2==0);
+console.log(filter1);
+
+
+//map: transform element
+const map1 = nums.map(n => n*2);
+console.log(map1);
