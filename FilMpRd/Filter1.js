5036e5b80640b0cadacd9416262333776dc6001c
shreyvyas <shreyvyas@gmail.com>
2026-01-10 17:56:39 +0530

    add reduce function implementation to calculate sum of array elements

--- FilMpRd/Filter1.js
+++ FilMpRd/Filter1.js
@@ -9,3 +9,15 @@ console.log(filter1);
 //map: transform element
 const map1 = nums.map(n => n*2);
 console.log(map1);
+
+//reduce: return one value e.g. sum of all elements
+const reduce1 = nums.reduce((acc, curr) => {
+    return acc+curr;
+}, 0);
+
+console.log(reduce1);
+
+//acc - stores the result
+//curr - current value picked from array
+//acc starts value will be 0 and 1 will be picked from array so it will be 0+1, now result is 1
+//now acc=1 and next curr value from array is 2 then 2+1=3
