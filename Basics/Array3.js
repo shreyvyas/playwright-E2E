25cd8de650db7ee73e90dca0177b6e3070fd80be
shreyvyas <shreyvyas@gmail.com>
2026-01-12 00:17:11 +0530

    add Array and Object examples for user data representation and iteration

---
+++ Basics/Array3.js
@@ -0,0 +1,10 @@
+const users = [
+
+    {name: "Shrey", age:37},
+    {name:"Ananta", age: 37},
+    {name:"Anagh", age:9},
+    {name:"Ekansh", age: 1}
+
+]
+
+
---
+++ Basics/Object1.js
@@ -0,0 +1,17 @@
+const users = {
+
+    name: "Shrey",
+    age: 37,
+    company: "Candescent"
+
+}
+
+console.log(users.name);
+
+for(let elem in users){
+
+    console.log(elem, users[elem]);
+
+}
+
+
